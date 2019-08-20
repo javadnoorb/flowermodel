3928d0f7502ae0c3800aebcdd9e97c965f79b7d2
javadnoorb <javad.noorbakhsh@gmail.com>
2019-08-20 17:55:05 -0400

    video RDF

--- flowermodel/flowermodel/calculate_rdf.py
+++ flowermodel/flowermodel/calculate_rdf.py
@@ -1,20 +1,55 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
+class __vidrdf__:
+    def __init__(self, blobfile, vidfile, nbins=100):
+        import imageio
+        self.nbins = nbins
+        if type(blobfile) == str:
+            self.blobs = pd.read_csv(blobfile)
+        else:
+            self.blobs = blobfile
+        if type(vidfile) == str:
+            self.vid = imageio.get_reader(vidfile,  'ffmpeg')
+        else:
+            self.vid = vidfile
+        self.num_frames = self.vid.count_frames()
+        (self.L1, self.L2, _) = self.vid.get_data(0).shape
+        self.maxL = min(self.L1, self.L2)       
 
-class framerdf:
-    def __init__(self, vid, blobs, image_idx, color1, color2, nbins=100):
-        blob = blobs[blobs['frame'] == image_idx]
+class vidrdf(__vidrdf__):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vid_qc()
+
+    def get_rdf(self, color1, color2):       
+        rdfs = []
+        for image_idx in tqdm(range(self.num_frames)):
+            rdf = framerdf(self.blobs, self.vid, image_idx, color1, color2)
+            rdfs.append(rdf.rdf)
+        rvals = rdf.rvals
+        return rvals, np.array(rdfs)
+
+    def vid_qc(self):
+        assert np.all(np.diff(np.array([self.vid.get_data(0).shape for n in range(self.num_frames)]), 
+                      axis=0) == 0), 'Some video frames have a different size'
+           
+        if self.L1!=self.L2: 
+            print('The images are not exactly squares. Using the minumum dimension for analysis')
+
+class framerdf(__vidrdf__):
+    def __init__(self, blobfile, vidfile, image_idx, color1, color2):
+        super().__init__(blobfile, vidfile)
+        
+        blob = self.blobs[self.blobs['frame'] == image_idx]
         self.X1 = blob.loc[blob['color'] == color1, ['x', 'y']].values
         self.X2 = blob.loc[blob['color'] == color2, ['x', 'y']].values
 
-
-#         (self.L1, self.L2, _) = vid.get_data().shape
-        
         self.get_blob_dists()
         self.get_cell_counts_for_rdf()
         
-        self.rvals, self.rdf = self.calculate_rdf(nbins=nbins)
+        self.rvals, self.rdf = self.calculate_rdf()
         
 
     def get_blob_dists(self):
@@ -42,8 +77,6 @@ class framerdf:
         some cells (with 'color2') may never be used in the analysis and hence are
         not counted toward the total count.
         ''' 
-
-        self.maxL = min(self.L1, self.L2)
         self.N = (self.dist <= self.maxL).sum()
 
     def calculate_point_rdf(self, r1, r2):
@@ -52,9 +85,9 @@ class framerdf:
         return rdf
     
     
-    def calculate_rdf(self, nbins=100):
+    def calculate_rdf(self):
 
-        rvals = np.linspace(0, self.maxL, nbins)
+        rvals = np.linspace(0, self.maxL, self.nbins)
 
         rdf = np.zeros_like(rvals)
         for idx in range(len(rvals)-1):
@@ -62,5 +95,5 @@ class framerdf:
 
         rvals = rvals[:-1]
         rdf = rdf[:-1]
-
-        return rvals, rdf
+        
+        return rvals, rdf      
