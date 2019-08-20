d01a1d226f323b0d2cea6ddd204b67a1f8f3ac36
javadnoorb <javad.noorbakhsh@gmail.com>
2019-08-20 12:34:43 -0400

    keep OOP

--- flowermodel/flowermodel/calculate_rdf.py
+++ flowermodel/flowermodel/calculate_rdf.py
@@ -1,48 +1,59 @@
-# import pandas as pd
+import pandas as pd
 import numpy as np
 
-def get_blob_dists(vid, blobs, image_idx, color1, color2):
-    '''
-    This function calculates pairwise distance between all blobs
-    of 'color1' to blobs of 'color2' assuming periodic boundary
-    conditions (only for one iteration).
-    '''
-    from scipy.spatial import distance_matrix
 
-    blob = blobs[blobs['frame'] == image_idx]
+class blobrdf:
+    def __init__(self, vid, blobs, image_idx, color1, color2):
+        print('get blob')
+        blob = blobs[blobs['frame'] == image_idx]
+        print('get X1, X2')
+        self.X1 = blob.loc[blob['color'] == color1, ['x', 'y']].values
+        self.X2 = blob.loc[blob['color'] == color2, ['x', 'y']].values
 
-    X1 = blob.loc[blob['color'] == color1, ['x', 'y']].values
-    X2 = blob.loc[blob['color'] == color2, ['x', 'y']].values
+#         self.vid_qc(vid)
+        print('get L1, L2')
+        (self.L1, self.L2, _) = vid.get_data(0).shape
+        if self.L1!=self.L2: 
+            print('The images are not exactly squares. Using the minumum dimension for analysis')
+        
+        print('get dist')
+        self.get_blob_dists()
+        print('get cell count ')
+        self.get_cell_counts_for_rdf()
+        
+    def vid_qc(self, vid):
+        assert np.all(np.diff(np.array([vid.get_data(0).shape for n in range(vid.count_frames())]), 
+                      axis=0) == 0), 'Some video frames have a different size'
 
-    (L1, L2, _) = vid.get_data(0).shape
-    dists = []
-    for i in [0, -L1, L1]:
-        for j in [0, -L2, L2]:
-            X2shift = X2 + [i, j]
-            dist = distance_matrix(X1, X2shift)
-            dists.append(dist)
-    dists = np.hstack(dists)
-    return dists
+    def get_blob_dists(self):
+        '''
+        This function calculates pairwise distance between all blobs
+        of 'color1' to blobs of 'color2' assuming periodic boundary
+        conditions (only for one iteration).
+        '''
+        from scipy.spatial import distance_matrix
 
-def get_cell_counts_for_rdf(vid, dist):
-    '''
-    This function finds the number of cells for RDF calculation, 
-    given the periodic boundary conditions, and the assumption that 
-    pairs of cells further than minimum dimension of the image 
-    should be ignored. Because of periodic boundary conditions 
-    some cells (with 'color2') may never be used in the analysis and hence are
-    not counted toward the total count.
-    ''' 
+        dists = []
+        for i in [0, -self.L1, self.L1]:
+            for j in [0, -self.L2, self.L2]:
+                X2shift = self.X2 + [i, j]
+                dist = distance_matrix(self.X1, X2shift)
+                dists.append(dist)
+        self.dist = np.hstack(dists)
 
-    (L1, L2, _) = vid.get_data(0).shape
+    def get_cell_counts_for_rdf(self):
+        '''
+        This function finds the number of cells for RDF calculation, 
+        given the periodic boundary conditions, and the assumption that 
+        pairs of cells further than minimum dimension of the image 
+        should be ignored. Because of periodic boundary conditions 
+        some cells (with 'color2') may never be used in the analysis and hence are
+        not counted toward the total count.
+        ''' 
 
-    if L1!=L2: 
-        print('The images are not exactly squares. Using the minumum dimension for analysis')
+        self.maxL = min(self.L1, self.L2)
+        self.N = (self.dist <= self.maxL).sum()
 
-    maxL = min(L1, L2)
-    N = (dist <= maxL).sum()
-    return N
-
-def calculate_rdf(dist, r1, r2, N):
-    rdf = ((r1 <= dist) & (dist < r2)).sum()/ N
-    return rdf
+    def calculate_rdf(self, r1, r2):
+        rdf = ((r1 <= self.dist) & (self.dist < r2)).sum()/ self.N
+        return rdf
