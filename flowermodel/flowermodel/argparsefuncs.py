81c5daddfecc32cc96f28f6030baff65a849ce18
javadnoorb <javad.noorbakhsh@gmail.com>
2019-08-22 09:26:16 -0400

    create pbs files

--- flowermodel/flowermodel/argparsefuncs.py
+++ flowermodel/flowermodel/argparsefuncs.py
@@ -14,3 +14,31 @@ def get_frame_blobs(args):
 def count_frames(args):
     vid = imageio.get_reader(args.filename,  'ffmpeg')
     print(vid.count_frames())
+
+def create_pbs(args):
+    vid = imageio.get_reader(args.filename,  'ffmpeg')
+    last_frame_index = vid.count_frames() - 1
+
+    pbs_text = '''#!/bin/bash
+#PBS -l nodes=1:ppn={num_cores}
+#PBS -l walltime={walltime}
+#PBS -l mem=4GB
+#PBS -q batch
+#PBS -N {jobname}
+#PBS -t 0-{last_frame_index}
+#PBS -o {pbslogs}/$PBS_JOBNAME.o
+#PBS -e {pbslogs}/$PBS_JOBNAME.e
+
+cd $PBS_O_WORKDIR
+FILE="{moviefile}"
+. /projects/chuang-lab/jnh/miniconda3/etc/profile.d/conda.sh
+conda activate flower
+flowermodel blob --filename $FILE --blob-index $PBS_ARRAYID
+'''.format(num_cores=args.num_cores, walltime=args.walltime, jobname=args.jobname, moviefile=args.filename,
+               last_frame_index=last_frame_index, pbslogs=args.pbslogs)
+
+    with open('{}.pbs'.format(args.jobname), 'w') as f:
+        f.write(pbs_text)
+    
+    if not os.path.exists(args.pbslogs):
+        os.makedirs(args.pbslogs)
