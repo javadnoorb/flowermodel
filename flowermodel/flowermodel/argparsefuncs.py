f44cacc13ded0d1e1763b60b04947bb7695c459a
javadnoorb <javad.noorbakhsh@gmail.com>
2019-09-03 10:49:07 -0400

    rename files/arg

--- flowermodel/flowermodel/argparsefuncs.py
+++ flowermodel/flowermodel/argparsefuncs.py
@@ -7,8 +7,8 @@ def get_frame_blobs(args):
     outputdir = os.path.join(args.out_dir, os.path.basename(args.filename))
     util.mkdir_if_not_exist(outputdir)
 
-    blobs = get_allframes_blobs(args.filename, min_idx=args.blob_index, max_idx=args.blob_index+1)
-    blobs.to_csv(os.path.join(outputdir, 'blob{:d}.csv'.format(args.blob_index)), index=False)
+    blobs = get_allframes_blobs(args.filename, min_idx=args.frame_index, max_idx=args.frame_index+1)
+    blobs.to_csv(os.path.join(outputdir, 'blob{:d}.csv'.format(args.frame_index)), index=False)
     
     
 def count_frames(args):
